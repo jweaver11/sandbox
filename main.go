409245557b915eb2d72cc6544436ee11b30dc0ce
Josh Weaver <jtw@digitalartbrokers.io>
2023-02-09 12:40:36 -0600

    Added fullscreen mode

--- main.go
+++ main.go
@@ -9,8 +9,14 @@ import (
 
 // Main function that runs the program
 func main() {
+	// Checks to see if the server is up first
+	// For now it just uses the charm server
+	if err := tea.NewProgram(CheckServerModel{}).Start(); err != nil {
+		fmt.Printf("There was an error fool: %v\n", err)
+		os.Exit(1)
+	}
 	// Sets p as a new tea program using out startUpModel
-	p := tea.NewProgram(startUpModel())
+	p := tea.NewProgram(createProjectViewModel(), tea.WithAltScreen()) //tea.WithAltScreen starts in fullscreen mode
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Error running the program: %v", err)
 		os.Exit(1)
